66e40f9eeb92b75a3edfb0e8fb0cc9d7f2c1f558
akka150000 <edj842768@gmail.com>
2025-02-07 15:31:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7826853926:AAFzhguIvFLEZPhmuCcpet9Cy4ATJg-paG8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("sibuna66")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
-
+ADMIN_IDS=("7186024164")
 
 
 
